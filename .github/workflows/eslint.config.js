60b852fe32b8dfc8d10d2038097665415f465be5
Skwaer17 <skwaer0817@gmail.com>
2025-01-27 15:20:57 +0100

    Code changed for singular object

--- .github/workflows/eslint.config.js
+++ .github/workflows/eslint.config.js
@@ -1,12 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,cjs,ts}"],
+  languageOptions: {
+    globals: globals.browser,
+  },
+  extends: [
+    pluginJs.configs.recommended,
+    tseslint.configs.recommended, // Ensure @typescript-eslint is installed 
+and configured
+  ],
+};
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
