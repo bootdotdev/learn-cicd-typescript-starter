e139e32d5a25059b16dba0a797892cf121a9a874
Piele <pieledecal@gmail.com>
2025-08-22 21:49:35 +0300

    Add format check

--- src/api/auth.test.ts
+++ src/api/auth.test.ts
@@ -2,8 +2,8 @@ import { describe, expect, test } from "vitest";
 import { getAPIKey } from "./auth";
 
 describe("getApiKey", () => {
-    test("return null when no authorization exists in header", () => {
-        const mockHeader = {};
-        expect(getAPIKey(mockHeader)).toBeFalsy();
-    })
-})
+  test("return null when no authorization exists in header", () => {
+    const mockHeader = {};
+    expect(getAPIKey(mockHeader)).toBeFalsy();
+  });
+});
