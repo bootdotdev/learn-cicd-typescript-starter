28013523e1f948e3c55f8d4a70851b3363d95d9e
Piele <pieledecal@gmail.com>
2025-08-22 21:34:18 +0300

    Added tests

---
+++ src/api/auth.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "./auth";
+
+describe("getApiKey", () => {
+    test("return null when no authorization exists in header", () => {
+        const mockHeader = {};
+        expect(getAPIKey(mockHeader)).toBeFalsy();
+    })
+})
