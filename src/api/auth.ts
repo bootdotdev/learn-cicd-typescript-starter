21a5a1295f1dd9216eff30c423381a199ac08bd4
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-02-25 18:54:46 +0300

    Testing: Intentionally Breaking Test to verify working Workflow

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,14 +3,12 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    console.log("trully empty");
     return null;
   }
 
   const trimmedAuth = authHeader.trim();
   const splitAuth = trimmedAuth.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    console.log("Not Api  key just another key ");
     return null;
   }
 
