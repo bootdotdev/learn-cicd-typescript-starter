fbf1f40c40834272a97e8cd820a2021a810e4476
a7mad1112 <ahmalawneh79@gmail.com>
2026-03-02 00:40:50 +0200

    e

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,13 +1,16 @@
-// src/api/auth.ts
 import { IncomingHttpHeaders } from "http";
 
-export function getAPIKey(headers: IncomingHttpHeaders | undefined): string | null {
-  // تأكد من أن headers موجودة
-  return "BROKEN";
-  const authHeader = headers?.authorization;
+export function getAPIKey(headers: IncomingHttpHeaders): string | null {
+  const authHeader = headers["authorization"];
   if (!authHeader) {
     return null;
   }
-  // إزالة "Bearer " من القيمة
-  return authHeader.replace("Bearer ", "");
+
+  const splitAuth = authHeader.split(" ");
+  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
+    return null;
+  }
+
+  return splitAuth[1];
 }
+
