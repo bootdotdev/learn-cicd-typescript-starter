14a895c401276cf1129a112ce46aee9406361197
Ruahuwari <ruahuwari@gmail.com>
2026-03-04 20:31:09 +0200

    failing lint test

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-
-type Config = {
-  db: DBConfig;
-  api: APIConfig;
-};
-
 type APIConfig = {
   port: string | undefined;
   filepathRoot: string;
@@ -14,6 +8,12 @@ type APIConfig = {
 type DBConfig = {
   url: string | undefined;
 };
+type Config = {
+  db: DBConfig;
+  api: APIConfig;
+};
+
+
 
 export const config: Config = {
   api: {
