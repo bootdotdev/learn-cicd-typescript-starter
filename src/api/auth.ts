9d862d19d36867c85337b88be6b9646164c5c0e9
adhamyaqoub <eng.adhamyaqoub@gmail.com>
2026-03-05 16:53:43 +0200

    chore: add Prettier formatting and scripts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,7 @@
 import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders | undefined): string {
-  
   const authHeader = headers?.authorization;
 
-  
   return typeof authHeader === "string" ? authHeader : "";
 }
