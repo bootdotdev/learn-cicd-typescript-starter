7468caae61d8e63c9dec3d73a6f4ffbf6c0e5e11
adhamyaqoub <eng.adhamyaqoub@gmail.com>
2026-03-05 16:26:14 +0200

    test: break getAPIKey to verify CI fails

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,15 +1,4 @@
-import { IncomingHttpHeaders } from "http";
-
-export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeader = headers["authorization"];
-  if (!authHeader) {
-    return null;
-  }
-
-  const splitAuth = authHeader.split(" ");
-  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    return null;
-  }
-
-  return splitAuth[1];
+import type { IncomingHttpHeaders } from "http";
+export function getAPIKey(headers: IncomingHttpHeaders | undefined): string {
+  return null as any; 
 }
