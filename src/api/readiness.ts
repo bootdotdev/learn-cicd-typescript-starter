8ebdfcdfbed7aa1a5c3e03588a2483c34d3f4598
Luizcs2 <Luizohto2012@gmail.com>
2026-03-06 11:12:17 +0000

    ch2:L4

--- src/api/readiness.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-import { respondWithJSON } from "./json.js";
-
-export function handlerReadiness(req: Request, res: Response) {
-  respondWithJSON(res, 200, { status: "ok" });
-}
