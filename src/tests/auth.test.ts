d996edf8f3a098197aedfaf6900e909932e54504
Luizcs2 <Luizohto2012@gmail.com>
2026-03-06 11:27:41 +0000

    ch3:L1

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,22 +2,22 @@ import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth";
 
 const http = {
-    "content-length": "123",
-    "content-type": "text/plain",
-    "connection": "keep-alive",
-    "host": "example.com",
-    "accept": "*" ,
-    "authorization": "ApiKey 1234567890abcdef"
-}
+  "content-length": "123",
+  "content-type": "text/plain",
+  connection: "keep-alive",
+  host: "example.com",
+  accept: "*",
+  authorization: "ApiKey 1234567890abcdef",
+};
 
 describe("get apikey", () => {
-    test("returns the api key from authorization header", () => {
-        const apikey = getAPIKey(http);
-        expect(apikey).toBe("1234567890abcdef");
-    });
+  test("returns the api key from authorization header", () => {
+    const apikey = getAPIKey(http);
+    expect(apikey).toBe("1234567890abcdef");
+  });
 
-    test("returns null if the apikey is missing",() => {
-        const apikey = getAPIKey({});
-        expect(apikey).toBeNull();
-    });
+  test("returns null if the apikey is missing", () => {
+    const apikey = getAPIKey({});
+    expect(apikey).toBeNull();
+  });
 });
