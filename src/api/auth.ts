c50e192e08cbb657c4727126e963b564cfa12f98
Lama Rimawi <1213515@student.birzeit.edu>
2026-03-07 20:34:37 +0200

    fix getAPIKey and verify passing CI

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,10 @@
 import { IncomingHttpHeaders } from "http";
 
-export function getAPIKey(headers: any): string | null {
-  return "broken"; 
-}
+export function getAPIKey(headers: IncomingHttpHeaders): string | null {
+  const authHeader = headers["authorization"];
+  if (!authHeader) {
+    return null;
+  }
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
     return null;
