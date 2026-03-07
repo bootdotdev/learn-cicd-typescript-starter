0c81130b7777a3546c5f1152c2ffc7a581a32c5a
Lama Rimawi <1213515@student.birzeit.edu>
2026-03-07 20:33:13 +0200

    add tests to CI and break code to test failure

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,8 @@
 import { IncomingHttpHeaders } from "http";
 
-export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeader = headers["authorization"];
-  if (!authHeader) {
-    return null;
-  }
-
+export function getAPIKey(headers: any): string | null {
+  return "broken"; 
+}
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
     return null;
