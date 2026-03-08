de6e34fbedac3098351c082c7dfeaf8f76af9e02
Lama Rimawi <1213515@student.birzeit.edu>
2026-03-08 15:27:37 +0200

    fix security warnings and ignore dist folder

--- eslint.config.js
+++ eslint.config.js
@@ -6,11 +6,14 @@ import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
+    ignores: ["dist/**"], // Tell ESLint to skip the dist folder
+  },
+  {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node } // Changed to node
+    languageOptions: { globals: globals.node }
   },
   tseslint.configs.recommended,
-  pluginSecurity.configs.recommended, // Added security config
+  pluginSecurity.configs.recommended,
 ]);
