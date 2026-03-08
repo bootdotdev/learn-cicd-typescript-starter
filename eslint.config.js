8692ae32a25b6f67d9fdc86ea395c79be1d1b318
sabriNoor <nooraosama63@gmail.com>
2026-03-08 13:54:19 +0200

    feat: add lint check

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    ignores: ["dist", "node_modules"],
+  },
+]);
