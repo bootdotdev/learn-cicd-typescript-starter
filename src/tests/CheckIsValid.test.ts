d476b725e5d775d32f891006e1e4e43cfff51cc1
ahmadissa22145-cell <ahmadissa22145@gmail.com>
2026-03-09 12:51:19 +0200

    add some tests

---
+++ src/tests/CheckIsValid.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeDefined();
