f145c8c6f6708e0a35b7e5ad291fff75f70d439e
Khaled-Masri0 <masrikhaled2003@gmail.com>
2026-03-10 22:25:35 +0200

    removed unused function

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist/"]
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error"
+    }
+  }
+];
