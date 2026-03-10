2e4cfa8a283ba498c8a0add58b0382792c9b404f
bashamahmoud <basha.mahmoud.2000@gmail.com>
2026-03-10 23:34:00 +0200

    edit warning limit

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import eslint-plugin-security
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
