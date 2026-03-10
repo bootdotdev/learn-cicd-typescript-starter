526b53cfe95f573063169f97f5b798a3b2b19cca
muhammad qzih <muhammadghqzih@gmail.com>
2026-03-11 01:03:47 +0200

    style: format eslint.config.ts and package.json for improved readability

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { ignores: ["dist/**"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   tseslint.configs.recommended,
 ]);
