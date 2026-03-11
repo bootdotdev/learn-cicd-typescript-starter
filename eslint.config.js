bec038b99477036543689b6c006c413fe033afb2
Mahmoud Arafat <mahmoudarafat0242@gmail.com>
2026-03-11 22:24:42 +0300

    Fix security lint warning

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default [
+export default defineConfig([
   {
     ignores: ["dist/**"],
   },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: {
-      globals: globals.node,
-    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
   },
-];
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+]);
