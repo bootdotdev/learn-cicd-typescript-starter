06e11d6b54a29ef464e4be0ed5bef1f0dc43940f
sammahkh <samahkhalid096@gmail.com>
2026-03-11 14:04:44 +0000

    Replace pseudoRandomBytes with randomBytes for security

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,9 @@ import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
+    ignores: ["dist", "node_modules"],
+  },
+  {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
@@ -15,5 +18,6 @@ export default defineConfig([
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
-  pluginSecurity.configs.recommended,
+  pluginSecurity.configs.recommended,    
+
 ]);
