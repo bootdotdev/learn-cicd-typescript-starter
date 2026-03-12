fed230b6b5319d990b5f73d4693a19ee443cbdfc
Zaid <dagbanbia@gmail.com>
2026-03-12 11:19:10 +0100

    added eslint-plugin - security

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   { 
@@ -17,6 +18,7 @@ export default defineConfig([
     } 
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
   {
     // Ignore specific directories
     ignores: [
