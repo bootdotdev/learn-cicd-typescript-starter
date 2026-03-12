4b01d534b902caa56d50733a6d170ad967a4f25c
tala.edaly <talaedaly188@gmail.com>
2026-03-12 03:11:54 +0200

    fix import extension for docker build

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, expect, test } from "vitest";
-import { getAPIKey } from "../api/auth";
+import { something } from "../api/auth.js";
 
 describe("getAPIKey", () => {
   test("returns API key when header is valid", () => {
