107e3d73cd20a0d27dc61b38ee38ab640c2faa89
Wafaa-Hamed <wafamnal9@gmail.com>
2026-03-13 22:16:16 +0200

    Fix getAPIKey function

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,3 +1,15 @@
+import { IncomingHttpHeaders } from "http";
+
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  return null; // كسرنا الكود عمدًا للتأكد أن CI يكتشف الفشل
+  const authHeader = headers["authorization"];
+  if (!authHeader) {
+    return null;
+  }
+
+  const splitAuth = authHeader.split(" ");
+  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
+    return null;
+  }
+
+  return splitAuth[1];
 }
