be56c880f28c1f83d234b7bcab5a62cc394d1d2e
Wafaa-Hamed <wafamnal9@gmail.com>
2026-03-13 22:42:00 +0200

    Add CI steps and unit tests

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,15 @@
+import { describe, test, expect } from "vitest";
+import { getAPIKey } from "../api/auth";
+import { IncomingHttpHeaders } from "http";
+
+describe("getAPIKey function with headers", () => {
+  test("returns null if headers do not have authorization", () => {
+    const headers: IncomingHttpHeaders = {};
+    expect(getAPIKey(headers)).toBeNull();
+  });
+
+  test("returns the API key if authorization header is correct", () => {
+    const headers: IncomingHttpHeaders = { authorization: "ApiKey MY_SECRET_KEY" };
+    expect(getAPIKey(headers)).toBe("MY_SECRET_KEY");
+  });
+});
