0ffab53798a96ed71897fc055878138048938a38
mohammad <modewahbeh88@gmail.com>
2026-03-13 16:19:26 +0200

    save vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8', // uses @vitest/coverage-v8
+      reporter: ['text'], // prints coverage to console
+    },
+  },
+})
