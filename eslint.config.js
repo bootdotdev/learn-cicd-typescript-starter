396714d0df473fc6f666a537a5221269f7bd7aad
Wafaa-Hamed <wafamnal9@gmail.com>
2026-03-14 22:41:38 +0200

    Update ESLint config and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default [
+export default defineConfig([
   {
+    
     ignores: ["dist/**", "node_modules/**"],
   },
 
+ 
   js.configs.recommended,
 
   ...tseslint.configs.recommended,
@@ -18,4 +22,6 @@ export default [
       },
     },
   },
-];
+
+  pluginSecurity.configs.recommended,
+]);
