73b996a2a90ca5a1b0dab3982ba07cb4104f0cae
dragon warrior <soborinphannara.simm22@gmail.com>
2026-03-16 15:07:19 +0700

    added new steps to CI job

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "crypto";
+// import { randomUUID } from "crypto";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
