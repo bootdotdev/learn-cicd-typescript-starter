1704439e01d3e5bf6f33f481e0e1b3d9b5e25b5a
muradhethnawi <your-email@example.com>
2026-03-29 15:24:39 +0300

    Add eslint-plugin-security

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-export default tseslint.config(
-  { ignores: ["dist/", "node_modules/"] },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
+
+export default defineConfig([
   {
-    languageOptions: {
-      globals: {
-        console: "readonly",
-        process: "readonly"
-      }
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "error"
-    }
-  }
-);
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+]);
