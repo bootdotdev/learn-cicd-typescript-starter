f8836e3d854c066edfb30dfdc7f99e748f8ccf2a
Mohammed-Zrirake <mohammed.zrirake01@gmail.com>
2026-07-27 15:11:04 +0100

    adding formatting job

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,7 +2,7 @@ import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
- 
+
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
     return null;
