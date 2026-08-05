11918e9fb94f13896a3b566dae60aa2e8adba25a
dheinsoo <daniel@DanielsPC.localdomain>
2026-08-05 16:14:08 +0200

    cd

--- src/tests/myfirsttest.test.ts
+++ src/tests/myfirsttest.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { getAPIKey } from "../api/auth";
+import { getAPIKey } from "../api/auth.js";
 
 test("gets key from header", () => {
   expect(getAPIKey({ authorization: "ApiKey myKey" })).toBe("myKey");
